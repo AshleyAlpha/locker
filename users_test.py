dc0ab8cb7cc1cf3f1c6cfcbcecb7d28d08ebb4f4
Wecode <mashleyalpha@gmail.com>
2019-02-11 19:05:56 +0200

    added find in user test py file

--- users_test.py
+++ users_test.py
@@ -60,6 +60,19 @@ class TestUsers(unittest.TestCase):
             test_user.save_user()
             self.assertEqual(len(Users.users_list),2)
 
+    def test_find_user_by_username(self):
+        '''
+        test to check if we can find a user by username and display information
+        '''
+
+        self.new_user.save_user()
+        test_user = Users("mashleyalpha@gmail.com","ashleyalpha","miahmamie") # new user
+        test_user.save_user()
+
+        found_user = Users.find_by_email("ashleyalpha")
+
+        self.assertEqual(found_user.email,test_user.email)
+
 
 if __name__ == '__main__':
     unittest.main()
