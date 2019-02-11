e40d1d227bafd3d8635e3fb1f6a09414edc73780
Wecode <mashleyalpha@gmail.com>
2019-02-11 19:06:43 +0200

    added finf by in user py file

--- users.py
+++ users.py
@@ -12,3 +12,17 @@ class Users:
         save_user method saves user objects into users_list
         '''
         Users.users_list.append(self)
+ @classmethod
+    def find_by_username(cls,username):
+        '''
+        Method that takes in a number and returns a user that matches that number.
+
+        Args:
+            username: username to search for
+        Returns :
+            username of person that matches the username.
+        '''
+
+        for user in cls.users_list:
+            if user.username == username:
+                return username
