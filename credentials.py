8293b54025f4814dbee7a8d149feb6490d9d7031
Wecode <mashleyalpha@gmail.com>
2019-02-11 16:18:08 +0200

    deleted elements from credential file

--- credentials.py
+++ credentials.py
@@ -9,13 +9,3 @@ class Users:
             username : New user username.
             password: New user password.
         '''
-class Credentials: 
-     def __init__(self, app_name, password):
-
-        '''
-        __init__ method that helps us define properties for our objects.
-
-        Args:
-            app_name: New user application  name.
-            password: New user application's password.
-        '''
