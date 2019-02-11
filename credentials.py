3cb05efdaef401fb35b0a9360fa873d531efacdd
Wecode <mashleyalpha@gmail.com>
2019-02-11 16:17:07 +0200

    added elements in users file

---
+++ credentials.py
@@ -0,0 +1,21 @@
+class Users:
+    def __init__(self,email, username, password):
+
+        '''
+        __init__ method that helps us define properties for our objects.
+
+        Args:
+            email: New user email.
+            username : New user username.
+            password: New user password.
+        '''
+class Credentials: 
+     def __init__(self, app_name, password):
+
+        '''
+        __init__ method that helps us define properties for our objects.
+
+        Args:
+            app_name: New user application  name.
+            password: New user application's password.
+        '''
---
+++ users.py
@@ -0,0 +1,10 @@
+class Credentials: 
+     def __init__(self, app_name, password):
+
+        '''
+        __init__ method that helps us define properties for our objects.
+
+        Args:
+            app_name: New user application  name.
+            password: New user application's password.
+        '''
