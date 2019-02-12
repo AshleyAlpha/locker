76bde20757763e07e591f8a0fea257c632f6568d
Wecode <mashleyalpha@gmail.com>
2019-02-12 10:43:59 +0200

    added all operations on both user and credential in run py

--- run.py
+++ run.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3.6
 from user import Users
-def create_user(emal,username,password):
+from credential import Credentials
+def create_user(email,username,password):
     '''
     Function to create a new user
+    '''
+    new_user = Users(email,username,password)
+    return new_user
+def create_credential(app_name,password):
+    '''
+    Function to create a new credential
     '''
     new_user = Users(emal,username,password)
     return new_user
@@ -12,3 +19,57 @@ def save_user(user):
     Function to save new user
     '''
     user.save_user()
+
+def save_credential(credential):
+    '''
+    Function to save new credential
+    '''
+    credential.save_credential()
+
+def del_user(user):
+    '''
+    Function to delete a user
+    '''
+    user.delete_user()
+
+def del_credential(credential):
+    '''
+    Function to delete a credential
+    '''
+    credential.delete_credential()
+
+def find_user(username):
+    '''
+    Function that finds a user by username and returns the user
+    '''
+    return user.find_by_username(username)
+
+def find_credential(app_name):
+    '''
+    Function that finds a credential by app_name and returns the credential
+    '''
+    return credential.find_by_app_nameapp_name
+
+def check_existing_user(username):
+    '''
+    Function that check if a user exists with that username and return a Boolean
+    '''
+    return Users.user_exist(username)
+
+def check_existing_credential(app_name):
+    '''
+    Function that check if a credential exists with that app_name and return a Boolean
+    '''
+    return Credentials.credential_exist(app_name)
+
+def display_user():
+    '''
+    Function that returns all the saved users
+    '''
+    return Users.display_user()
+
+def display_credential():
+    '''
+    Function that returns all the saved credentials
+    '''
+    return Credentials.display_credential()
