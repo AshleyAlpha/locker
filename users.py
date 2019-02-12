1e8849f691b65c41675f110493e530f58995b2c3
Wecode <mashleyalpha@gmail.com>
2019-02-12 09:35:10 +0200

    test to find if the user exists in users py

--- users.py
+++ users.py
@@ -26,3 +26,18 @@ class Users:
         for user in cls.users_list:
             if user.username == username:
                 return user
+    @classmethod
+    def user_exist(cls,username):
+        '''
+        Method that checks if a user exists from the user list.
+        Args:
+            username: username to search if it exists
+        Returns :
+            Boolean: True or false depending if the user exists
+        '''
+        for user in cls.users_list:
+            if user.username == username:
+                    return True
+
+        return False
+            
